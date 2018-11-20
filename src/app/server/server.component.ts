9c5a30217fa19719a8e4c6767825653cf69242db
Jeremy <jeremyneilson31@gmail.com>
2018-11-20 14:41:47 -0700

    server component updates

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.scss']
+})
+
+export class ServerComponent {
+
+serverId = 10;
+serverStatus = 'offline';
+
+getServerStatus() {
+    return this.serverStatus;
+    }
+}
+
+
